cfbea89efc68b4b470fb7bf0671e1041a9c3fb8d
Taylor Parrish <twpearish@gmail.com>
2018-06-02 23:09:53 +0200

    added user model, POST route and minor validation to user endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+// Load user model
+const User = require('../../models/User');
 
 // @route    GET api/users/test
 // @desc     Tests POST route
 // @access   Public
 router.get('/test', (req, res) => res.json({msg: 'user works'}));
+
+// @route    POST api/users/register
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user) {
+                return res.status(400).json({email: 'Email already exists'});
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar, 
+                    password: req.body.password
+                })
+            }
+        })
+})
 
 module.exports = router;
