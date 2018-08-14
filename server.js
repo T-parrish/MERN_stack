9f71a428e3c0de0ae64942d74f70d464dcd89dda
Taylor Parrish <twpearish@gmail.com>
2018-08-14 12:51:13 -0700

    bug hunting

--- server.js
+++ server.js
@@ -37,13 +37,14 @@ app.use('/api/profile', profile)
 app.use('/api/posts', posts)
 
 // Serve static assets if in production
-if(process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-
-    app.get('*' , (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
 
 
 const port = process.env.PORT || 5000;
