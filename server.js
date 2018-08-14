90900e2a8f91ba2e7318d4a946501de69e2109cf
Taylor Parrish <twpearish@gmail.com>
2018-08-14 12:26:20 -0700

    final changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 // Models
 const users = require('./routes/api/users');
@@ -35,7 +36,17 @@ app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
 
+// Serve static assets if in production
+if(process.env.NODE_ENV = 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*' , (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`))
+
